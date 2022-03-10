cfbced00966380208395061482b0bc837b6f1eea
parhampr <kamal20012011@hotmail.com>
2022-03-10 12:37:54 +0400

    Network changes

--- Application/TodoBackend/models/Network.model.js
+++ Application/TodoBackend/models/Network.model.js
@@ -4,16 +4,32 @@ const { Schema } = mongoose;
 const Block_Network = mongoose.model(
     "Block_Network",
     new Schema({
-        Name: {type: String, required: [true, 'Network name cannot be empty'], trim: true},
+        Name: {
+            type: String,
+            validate: {
+                validator: (v)=> /^[a-z ,.'-]+$/.test(v),
+                message: props => `${props.value} is not a valid network name!\nA valid network ID contains no spaces, no special characters in the beginning or end. \nAllowed special characters include 'underscore' and 'dot'`
+            },
+            required: [true, 'Network ID cannot be empty'],
+            trim: true},
+        NetID: {
+            type: String,
+            validate: {
+                validator: (v)=> /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/.test(v),
+                message: props => `${props.value} is not a valid network ID!\nA valid network ID contains no spaces, no special characters in the beginning or end. \nAllowed special characters include 'underscore' and 'dot'`
+            },
+            required: [true, 'Network ID cannot be empty'],
+            trim: true},
         Address: {
             type: String,
             validate: {
                 validator: (v)=> /^[a-vx-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(v),
-                message: props => `${props.value} is not a valid organisation address!`
+                message: props => `${props.value} is not a valid organisation address!\nFor valid blockchain address please remove http|https:// and www., if any`
             },
             required: [true, "Network Address cannot be empty"],
             trim: true
         },
+        // LEFT HERE
         Organizations: [Number]
     },
     {timestamps: true})
--- Application/TodoBackend/models/Organization.model.js
+++ Application/TodoBackend/models/Organization.model.js
@@ -1,2 +1,9 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
+
+const Organizations = mongoose.model(
+    "Organizations", new Schema({
+        Name: {type: String, required: [true, 'Organization name is required'], trim: true},
+        AdminID: {type: String, required: [true, "Admin username is required"], trim: true}
+    },
+    {timestamps: true}));
