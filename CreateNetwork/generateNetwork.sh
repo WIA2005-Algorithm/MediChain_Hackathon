9ce8d9facc06a6dc5fdc787d02eefaeb6d6f08a2
parhampr <kamal20012011@hotmail.com>
2022-03-24 01:52:44 +0400

    Completed Part 1

--- CreateNetwork/generateNetwork.sh
+++ CreateNetwork/generateNetwork.sh
@@ -103,7 +103,7 @@ while [[ $# -gt 0 ]]; do
       COUNTRY+=(`echo ${5// /#{S\}}`)
       STATE+=(`echo ${6// /#{S\}}`)
       LOCATION+=(`echo ${7// /#{S\}}`)
-      echo $LOCATION > lol.html
+      echo $LOCATION
       P0PORT+=("$8")
       CAPORT+=("$9")
       COUCHPORT+=("${10}")
